de73e9dfd1c7eed321a70d24e805a41eab4d8981
sadanobu.mizusako <sadanobu.mizusako@gmail.com>
2024-08-17 22:52:21 +0900

    Add init

--- confest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-from pathlib import Path
-
-prj_path = str(Path(__file__).resolve().parent)
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), prj_path)))
